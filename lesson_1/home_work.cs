a02e5bcc1c9423736c0a8488a824e6924a8f5e1d
Viktoria Hrechuk <380666840376vika@gmail.com>
2022-10-25 00:02:57 +0300

    Add the lesson 1 homework

---
+++ lesson_1/home_work.cs
@@ -0,0 +1,13 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+        int x = int.Parse(Console.ReadLine());
+        int y = int.Parse(Console.ReadLine());
+        int result = x + y;
+        
+		Console.WriteLine("{0} + {1} = {2}", x, y, result);
+	}	
+}
